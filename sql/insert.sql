d4b470c72ce61242f4fe9ea20f3720c9d1620b98
eaishwa <av2845@columbia.edu>
2019-03-10 13:09:06 -0400

    corrected comments and replies

--- sql/insert.sql
+++ sql/insert.sql
@@ -276,60 +276,108 @@ INSERT INTO Comments (postId, commentId, uni, content) VALUES
 (
 	5,
 	6,
-	'alk2225'.
+	'alk2225',
 	'Researching on educational institutions is great because it uncovers a lot of truth.'
+),
+(
+	6,
+	17,
+	'nr2409',
+	'I wanna get started at NLP. Can I contact you privately?'
+),
+(
+	6,
+	18,
+	'yw3201',
+	'Sure, here is my email id - yw3201@columbia.edu'
+),
+(
+	7,
+	19,
+	'xh2389',
+	'Nope. Interest is what matters!!'
+),
+(
+	7,
+	20,
+	'cb3441',
+	'Is prior knowledge in finance required to apply for the company you are working for?'
+),
+(
+	7,
+	21,
+	'cb3441',
+	'Are there any openings for entry level data science internships in your company?'
+),
+(
+	7,
+	22,
+	'xh2389',
+	'I am not sure at this point, I shall get back once I check with my team'
+),
+(
+	7,
+	23,
+	'cb3441',
+	'Thanks a lot for the information and please let me know if any opening is available'
+),
+(
+	7,
+	25,
+	'av2845',
+	'Please let me also know if there are openings. I am also interested'
 )
 ;
 
 INSERT INTO Replies (commentId, replyId, postId) VALUES
 (
 	1,
-	1,
+	15,
 	1
 ),
 (
 	2,
-	2,
+	14,
 	2
 )
 (
-	6,
 	3,
-	5
-),
-(
-	26,
 	4,
-	5
-),
-(
-	5,
-	5,
-	4
-),
-(
-	4,
-	6,
 	3
 ),
 (
 	16,
-	7,
+	5,
 	4
 ),
 (
-	14,
-	8,
-	2
+	26,
+	6,
+	5
 ),
 (
-	15,
-	9,
-	1
+	17,
+	18,
+	6
 ),
 (
-	3,
-	10,
-	3
+	20,
+	19,
+	7
+),
+(
+	21,
+	22,
+	7
+),
+(
+	22,
+	23,
+	7
+),
+(
+	22,
+	25,
+	7
 )
 ;
