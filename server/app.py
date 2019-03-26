06dfda0e58f3db9cf228fa79afbc667b75bc3721
Terry1004 <hym961004@gmail.com>
2019-03-26 15:17:38 -0400

    Add app run method call so one can directly execute the python file to start a web server

--- server/app.py
+++ server/app.py
@@ -16,3 +16,7 @@ app.register_blueprint(profile.blueprint)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000, debug=True, threaded=True)
