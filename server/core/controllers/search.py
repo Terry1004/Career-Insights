75b4343dead4e31601fffd8228335e50a29aa958
eaishwa <av2845@columbia.edu>
2019-04-10 12:55:44 -0400

    search utility errors fixed

--- server/core/controllers/search.py
+++ server/core/controllers/search.py
@@ -6,20 +6,25 @@ from ..models.post import Post
 
 bluePrint = Blueprint('search', __name__, url_prefix='/search')
 
-@bluePrint.route('/search', methods=['GET'])
+
+@bluePrint.route('/', methods=['GET','POST'])
 def detail():
-    uni = request.args.get('uni')
-    name = request.args.get('name')
-    company = request.args.get('company')
-    keywords = request.args.get('keywords')
-    title = request.args.get('title')
+    if request.method == 'GET':
+        return render_template('search/search.html')
 
-    post1 = Post.find_by_company(company)
-    post2 = Post.find_by_name(name)
-    post3 = Post.find_from_posts(uni, title, keywords)
-    all_posts = post1+post2+post3
+    if request.method == 'POST':
+        uni = request.args.get('uni')
+        name = request.args.get('name')
+        company = request.args.get('company')
+        keywords = request.args.get('keywords')
+        title = request.args.get('title')
 
-    if all_posts:
-        return render_template('post/index.html', post=all_posts)
-    else:
-        return render_template('error/404.html', message='No Posts Found relevant to search')
+        post1 = Post.find_by_company(company)
+        post2 = Post.find_by_name(name)
+        post3 = Post.find_from_posts(uni, title, keywords)
+        all_posts = post1+post2+post3
+
+        if all_posts:
+            return render_template('post/index.html', post=all_posts)
+        else:
+            return render_template('error/404.html', message='No Posts Found relevant to search')
